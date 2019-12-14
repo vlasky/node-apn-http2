138f6d6937d68dd6e771985785354efafae668ce
Vlad Lasky <vlasky@users.noreply.github.com>
2019-12-14 21:38:23 +1100

    Added support for the apns-push-type attribute, now required since the introduction of iOS 13.
    Other changes include code to correctly set the apns-priority attribute value.
    
    References:
    
    https://onesignal.com/blog/ios-13-introduces-4-breaking-changes-to-notifications/
    https://developer.apple.com/documentation/usernotifications/setting_up_a_remote_notification_server/sending_notification_requests_to_apns?language=objc

--- src/APNNotification.ts
+++ src/APNNotification.ts
@@ -4,7 +4,8 @@ export class APNNotification extends APNNotificationBase {
   encoding = 'utf8';
   compiled: string = null;
   expiry = 0;
-  priority = 10;
+  priority: number = null;
+  pushType: string = null;
   topic: string = null;
   collapseId: string = null;
   id: string = null;
@@ -21,8 +22,29 @@ export class APNNotification extends APNNotificationBase {
   headers() {
     let headers = {};
 
-    if (this.priority !== 10) {
-      headers["apns-priority"] = this.priority;
+    if (this.priority) {
+      //The default APN priority is 10, so if it's set to that, we will leave it out of the header to save space
+      if (this.priority !== 10) {
+        headers["apns-priority"] = this.priority;
+      }
+      //If this notification is an alert or badge or sound, the priority will also be left at the default value 
+    } else if (this.aps.alert || this.aps.badge || this.aps.sound) {
+      //Apple's new rules for iOS 13 require content-available notifications to have a priority of 5
+    } else if (this.aps["content-available"] === 1) {
+      headers["apns-priority"] = 5;
+    }
+
+    if (this.pushtype) {
+      //If a pushType has been provided, use that
+      headers["apns-push-type"] = this.pushType;
+      //Anything with an alert or badge or sound is considered an alert
+    } else if (this.aps.alert || this.aps.badge || this.aps.sound) {
+      headers["apns-push-type"] = "alert";
+      //Otherwise, Apple's new rules for iOS 13 require pure content-available notifications to have a push type of background 
+    } else if (this.aps["content-available"] === 1) {
+      headers["apns-push-type"] = "background";
+    } else {
+      console.warn("APNNotification.ts::headers(): pushType not specified for APN notification. Might not be relayed by Apple.");
     }
 
     if (this.id) {
